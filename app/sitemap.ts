2e45d8c76eb0c35bf7052675dd8d3ec05d2d82a8
Ilias Ismanalijev <me@il.ly>
2023-10-15 00:38:27 +0200

    x

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,17 @@
+import { getAllPostsForHome } from '@/lib/api'
+
 const domain = `swissobserver.com`
 
 export default async function Sitemap() {
+  const posts = await getAllPostsForHome()
   return [
     {
       url: `https://${domain}`,
       lastModified: new Date(),
     },
+    ...posts.map((post) => ({
+      url: `https://${domain}/${post.node.slug}`,
+      lastModified: new Date(post.node.date),
+    })),
   ]
 }
