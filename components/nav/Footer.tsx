ff43346cca8cbb36c21d945b1f6ed19e1c3c0812
Ilias Ismanalijev <me@il.ly>
2023-11-02 13:45:13 +0100

    sizes

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -27,6 +27,7 @@ export function Footer() {
           <NavLink href="/pricing">Pricing</NavLink>
           <NavLink href="/faq">FAQ</NavLink>
           <NavLink href="/best-practices">Best Practices</NavLink>
+          <NavLink href="/sizes">Image Sizes</NavLink>
           <div className="flex-1"></div>
           <Link
             href="https://magicspace.ae"
