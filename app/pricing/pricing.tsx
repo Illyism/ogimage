3d2d200983bc31588dfce31eb8611d35d28717ff
Ilias Ismanalijev <me@il.ly>
2023-11-02 13:48:57 +0100

    space

--- app/pricing/pricing.tsx
+++ app/pricing/pricing.tsx
@@ -73,7 +73,7 @@ const pricingItems = [
 
 const Pricing = () => {
   return (
-    <div className="contain mb-8 text-center">
+    <div className="contain my-8 text-center">
       <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
         {pricingItems.map(({ plan, tagline, quota, features, cta }) => {
           const price =
