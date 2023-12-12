b031b79a2af99e1ce0bf482df34112c8bf28fb1a
Ilias Ismanalijev <me@il.ly>
2023-12-12 14:51:14 +0100

    Update OG Image Inspiration page layout and content

--- app/inspiration/page.tsx
+++ app/inspiration/page.tsx
@@ -4,18 +4,23 @@ import { getLatestInspiration } from '@/lib/directus'
 import Link from 'next/link'
 import { ImageCard } from './post/[slug]/ImageCard'
 
+export const revlidate = 5 * 60 // 5 minutes
+
 export default async function Page() {
   const list = await getLatestInspiration()
   return (
     <PageLayout>
-      <div className="contain pt-4 lg:pt-16">
+      <div className="pad pt-4 lg:pt-16">
         <h1 className="mb-2 text-2xl font-bold text-gray-900 sm:text-3xl">
-          Latest OG Image Inspiration
+          The Best OG Image Inspiration
         </h1>
         <p className="mb-8 text-base font-normal leading-7 text-gray-600">
-          Get inspired by the latest OG Image designs.
+          Need open graph image inspiration? We&apos;ve got you covered.
+          Discover the best OG image designs, templates, and more at
+          OGimage.org. Be inspired by handpicked examples of real OG images,
+          ensuring the highest quality.
         </p>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {list.map((item, i) => (
             <Link key={i} href={`/inspiration/post/${item.slug}`}>
               <ImageCard
@@ -23,9 +28,11 @@ export default async function Page() {
                 alt={`OG Image for ${item.domain}`}
                 color={item.color[0]}
               />
-              <div>
-                <h2 className="text-lg font-bold">{item.domain} OG Image</h2>
-                <p className="truncate text-sm text-gray-600">{item.name}</p>
+              <div className="flex items-center justify-between">
+                <div className="text-lg font-bold">{item.name}</div>
+                <div className="truncate text-sm text-gray-600">
+                  {item.domain}
+                </div>
               </div>
             </Link>
           ))}
