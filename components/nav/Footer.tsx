f9fc111773f00243118c3800a52d2d11017248b8
Ilias Ismanalijev <me@il.ly>
2023-12-16 12:47:35 +0100

    Add social links and update footer design

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -1,73 +1,145 @@
-import { cn } from '@/lib/utils'
-import { Sparkles, SwitchCamera } from 'lucide-react'
 import Link from 'next/link'
+import { TestimonialReviews } from '../reviews/testimonial-reviews'
+import { Button } from '../ui/button'
 
-export function Footer() {
-  const now = new Date()
-
+export const Footer = () => {
   return (
-    <footer className="mt-16 w-full">
-      <div className="contain border-t-2 border-double py-2">
-        <Link
-          href="/"
-          className="inline-flex items-center gap-1.5 font-display text-lg font-black lg:text-2xl"
-        >
-          <div className="mt-1 flex h-6 w-7 items-center justify-center rounded-lg bg-primary text-white">
-            <SwitchCamera className="h-4 w-4" />
+    <footer className="contain py-12 xl:py-16">
+      <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
+        <div className="flex flex-col items-center justify-between gap-8 sm:flex-row">
+          <div className="inline-flex items-center gap-2 text-lg font-bold leading-none">
+            <div>
+              <Link
+                href="/"
+                className="mb-2 text-lg font-bold leading-none hover:text-indigo-500"
+              >
+                OGimage.org
+              </Link>
+              <div className="text-sm leading-none opacity-80 transition-opacity duration-150 ease-in-out hover:opacity-100">
+                OG Image Generator
+              </div>
+            </div>
           </div>
-          OGimage.org
-        </Link>
-      </div>
-      <div>
-        <nav className="contain flex items-center border-b-2 border-double text-xs">
-          <NavLink href="/" className="-ml-2">
-            Home
-          </NavLink>
-          <NavLink href="/templates">Templates</NavLink>
-          <NavLink href="/pricing">Pricing</NavLink>
-          <NavLink href="/faq">FAQ</NavLink>
-          <NavLink href="/best-practices">Best Practices</NavLink>
-          <NavLink href="/sizes">Image Sizes</NavLink>
-          <NavLink href="/for-blog">For blogs</NavLink>
-          <div className="flex-1"></div>
-          <Link
-            href="https://magicspace.agency"
-            aria-label="MagicSpace"
-            title="MagicSpace"
-          >
-            <Sparkles className="h-4 w-4" />
-          </Link>
-        </nav>
-      </div>
-      <div className="contain flex items-center justify-between border-double text-xs">
-        <div>© {now.getFullYear()} OGimage.org</div>
-        <NavLink href="/about">About Us</NavLink>
-        <NavLink href="/privacy">Privacy Policy</NavLink>
-        <div className="flex-1"></div>
-        <NavLink href="/how-to-change-link-preview-image-facebook">
-          Facebook
-        </NavLink>
-        <NavLink href="/contact">Contact Us</NavLink>
+
+          <TestimonialReviews />
+        </div>
+
+        <div className="mt-8 flex flex-col items-start gap-4 text-black/80 sm:flex-row sm:gap-8 lg:gap-24">
+          <div className="grid w-full gap-1 text-sm font-bold sm:w-auto">
+            <div className="mb-2 ml-0.5 px-8 font-black uppercase sm:px-0 sm:text-xs">
+              OG Image Generator
+            </div>
+            <Link
+              href="/buy"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              🛒 Order Now
+            </Link>
+            <Link
+              href="/templates"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              🎨 OG Image Templates
+            </Link>
+            <Link
+              href="/inspiration"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              💡 OG Image Gallery
+            </Link>
+            <Link
+              href="/faq"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              ❓ FAQ
+            </Link>
+            <Link
+              href="https://magicspace.agency"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📈 SEO Services
+            </Link>
+          </div>
+
+          <div className="grid w-full gap-1 text-sm font-bold sm:w-auto">
+            <div className="mb-2 ml-0.5 px-8 font-black uppercase sm:px-0 sm:text-xs">
+              OG Image Resources
+            </div>
+
+            <Link
+              href="/best-practices"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📖 OG Image Best Practices
+            </Link>
+            <Link
+              href="/sizes"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📏 OG Image Sizes
+            </Link>
+            <Link
+              href="/for-blog"
+              target="_blank"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📰 OG Images for Blogs
+            </Link>
+            <Link
+              href="/how-to-change-link-preview-image-facebook"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:mb-2 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📰 Facebook Link Previews
+            </Link>
+          </div>
+
+          <div className="grid w-full gap-1 text-sm font-bold sm:w-auto">
+            <div className="mb-2 ml-0.5 px-8 font-black uppercase sm:px-0 sm:text-xs">
+              Our Company
+            </div>
+            <Link
+              href="/about"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              🤝 About Us
+            </Link>
+            <Link
+              href="/privacy"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📜 Privacy Policy
+            </Link>
+            <Link
+              href="https://il.ly/blog"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📝 SEO Blog
+            </Link>
+
+            <Link
+              href="/contact"
+              className="px-8 py-2 hover:bg-indigo-500/10 hover:text-indigo-500 sm:rounded sm:px-1 sm:py-0.5"
+            >
+              📞 Contact Us
+            </Link>
+          </div>
+        </div>
+
+        <div className=" mt-12 flex flex-col gap-8 sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex items-center justify-center gap-3 text-sm leading-tight text-yellow-400/80 sm:flex-row">
+            <Button
+              className="blackspace-nowrap h-auto w-fit cursor-pointer items-center justify-center gap-2 overflow-hidden rounded-2xl border border-primary bg-primary px-4 py-2.5 text-sm font-bold leading-none text-primary-foreground shadow-xl outline outline-1 outline-offset-[-2px] outline-primary-foreground/30 transition-all duration-150 ease-in-out hover:bg-primary/90"
+              asChild
+            >
+              <Link href="/buy">Get Started</Link>
+            </Button>
+          </div>
+
+          <div className="flex items-center justify-center text-sm">
+            &copy; {new Date().getFullYear()} OGimage.org
+          </div>
+        </div>
       </div>
     </footer>
-  )
-}
-
-const NavLink = ({
-  href,
-  children,
-  className,
-}: {
-  href: string
-  children: React.ReactNode
-  className?: string
-}) => {
-  return (
-    <Link
-      href={href}
-      className={cn('px-2 py-2 font-medium hover:bg-card/50', className)}
-    >
-      {children}
-    </Link>
   )
 }
