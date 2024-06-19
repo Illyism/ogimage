339a5044d772ab84a285b480e02c13e646f0166e
Ilias Ismanalijev <me@il.ly>
2024-06-19 15:25:36 +0200

    chore: Add Product Hunt launch announcement to PageLayout

--- components/nav/PageLayout.tsx
+++ components/nav/PageLayout.tsx
@@ -6,6 +6,13 @@ export const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
+      <a
+        className="block w-full bg-[#ff6154] py-2 text-center text-xs font-black text-white transition hover:bg-orange-400"
+        href="https://www.producthunt.com/products/seo-roast"
+        target="_blank"
+      >
+        Launching soon on Product Hunt! - <u>Get Notified</u>
+      </a>
       <main className="flex-auto">{children}</main>
       <Footer />
     </>
