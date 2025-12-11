e07db4c11cd09d5797d17f5bad26666507c6f0e4
Ilias Ism <me@il.ly>
2025-12-11 15:04:06 +0100

    Update configuration files for ESLint and Prettier; add .prettierignore and .prettierrc.json for formatting rules. Modify .gitignore to include cache files and remove biome.json. Enhance package.json scripts for linting and formatting checks.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,20 @@
-import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
-import nextTypescript from 'eslint-config-next/typescript'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier/flat'
 
-const eslintConfig = [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-  {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-  },
-]
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
 
 export default eslintConfig
