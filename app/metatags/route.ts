5e5d1912b4369ae0accc670dd157551b1b2cfc55
Ilias Ism <me@il.ly>
2025-12-11 16:48:57 +0100

    Remove Upstash rate limiting functionality and related imports to streamline the codebase. Update package.json to reflect the removal of Upstash dependencies, enhancing maintainability and reducing complexity.

--- app/metatags/route.ts
+++ app/metatags/route.ts
@@ -1,28 +1,11 @@
-import { LOCALHOST_IP } from '@/lib/constants'
 import { getMetaTags } from '@/lib/metatags'
-import { ratelimit } from '@/lib/upstash'
 import { isValidUrl } from '@/lib/utils'
-import { ipAddress } from '@vercel/edge'
-import { getToken } from 'next-auth/jwt'
 import { NextRequest } from 'next/server'
 
 export const GET = async (req: NextRequest) => {
   const url = req.nextUrl.searchParams.get('url')
   if (!url || !isValidUrl(url)) {
     return new Response('Invalid URL', { status: 400 })
-  }
-
-  // Rate limit if user is not logged in
-  const session = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  })
-  if (!session?.email) {
-    const ip = ipAddress(req) || LOCALHOST_IP
-    const { success } = await ratelimit().limit(ip)
-    if (!success) {
-      return new Response("Don't DDoS me pls 🥺", { status: 429 })
-    }
   }
 
   const metatags = await getMetaTags(url)
